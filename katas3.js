510e1bc2fa5d03a08da84b880c0696d7dc7ff23c
rhamo1 <rhamo1@outlook.com>
2021-07-20 12:17:53 -0300

    Reenvio corrigido. Possíveis melhoraas?

--- katas3.js
+++ katas3.js
@@ -1,258 +1,378 @@
 const sampleArray = [469, 755, 244, 245, 758, 450, 302, 20, 712, 71, 456, 21, 398, 339, 882, 848, 179, 535, 940, 472];
 
 
-function showResults(n) {
-    let div = document.createElement('div');
-    div.textContent = n;
-    resultado.appendChild(div);
+function showResults(nome, arrContagem) {
+
+
+    let novoKata = document.getElementById('conteudo') // Pega id
+
+    let elemento = document.createElement('div'); // Cria div para resultados
+    let paragrafo = document.createElement('p'); // Cria p para resultados
+    let resultado = document.createTextNode(arrContagem) // Insere a contagem no elemento
+    let subTitulo = document.createElement('h3'); // Dá nome de acordo com o kata
+    let texto = document.createTextNode(nome); // Insere nome
+
+    subTitulo.appendChild(texto); // Insere nome do kata no h3 criado
+    elemento.appendChild(subTitulo); // Insere h3 na div
+    paragrafo.appendChild(resultado); // Insere resultados no p
+    elemento.appendChild(paragrafo); // Insere p na div
+
+    novoKata.appendChild(elemento) // Insere todos acima na página
 }
 
+// Resolução
+
+function kata1() {
+
+    let nome = 'Kata 1'
+    let arrContagem = []
 
 
-function kata1(n) {
-    showResults('kata1');
+    for (let i = 1; i <= 25; i++) {
+        arrContagem.push(' ' + (i));
 
-    for(let i=1; i<=25; i++){
-     showResults(i);
-        
     }
 
-} kata1();
+    showResults(nome, arrContagem)
+    return arrContagem
+
+}
+kata1();
+
 
 
 function kata2() {
 
-    showResults('kata2');
+    let nome = 'Kata 2'
+    let arrContagem = []
 
-    for(let i=25; i>=1; i--){
-        showResults(i);
+    for (let i = 25; i >= 1; i--) {
+        arrContagem.push(' ' + (i));
     }
-} kata2();
+
+    showResults(nome, arrContagem)
+    return arrContagem
+
+}
+kata2();
+
+
 
 function kata3() {
 
-    showResults('kata3');
+    let nome = 'Kata 3'
+    let arrContagem = []
 
-    for(let i=-1; i>=-25; i--){
-        showResults(i);
+    for (let i = -1; i >= -25; i--) {
+        arrContagem.push(' ' + (i));
     }
-} kata3();
+
+    showResults(nome, arrContagem)
+    return arrContagem
+
+}
+kata3();
+
+
 
 function kata4() {
 
-    showResults('kata4');
+    let nome = 'Kata 4'
+    let arrContagem = []
 
-    for(let i=-25; i<=-1; i++){
-        showResults(i);
+    for (let i = -25; i <= -1; i++) {
+        arrContagem.push(' ' + (i));
     }
 
-} kata4();
+    showResults(nome, arrContagem)
+    return arrContagem
+
+}
+kata4();
+
+
 
 function kata5() {
 
-    showResults('kata5');
+    let nome = 'Kata 5'
+    let arrContagem = []
 
-    for(let i=25; i>=-25; i--){
+    for (let i = 25; i >= -25; i--) {
 
-        if(i % 2 !== 0){
-        showResults(i);
+        if (i % 2 !== 0) {
+            arrContagem.push(' ' + (i));
+
         }
     }
 
-} kata5();
+    showResults(nome, arrContagem)
+    return arrContagem
+
+}
+kata5();
+
+
 
 function kata6() {
 
-    showResults('kata6');
+    let nome = 'Kata 6'
+    let arrContagem = []
 
-    for(let i=3; i<=100; i++){
+    for (let i = 3; i <= 100; i++) {
 
-        if(i % 3 === 0){
-        showResults(i);
+        if (i % 3 === 0) {
+            arrContagem.push(' ' + (i));
+
         }
     }
-    
-} kata6();
+
+    showResults(nome, arrContagem)
+    return arrContagem
+
+}
+kata6();
+
+
 
 function kata7() {
 
-    showResults('kata7');
+    let nome = 'Kata 7'
+    let arrContagem = []
 
-    for(let i=1; i<=100; i++){
+    for (let i = 1; i <= 100; i++) {
 
-        if(i % 7 === 0){
-        showResults(i);
+        if (i % 7 === 0) {
+            arrContagem.push(' ' + (i));
+
         }
     }
 
-} kata7();
+}
+kata7();
+
+
 
 function kata8() {
 
-    showResults('katas8');
+    let nome = 'Kata 8'
+    let arrContagem = []
 
-    for(let i=100; i>=3; i--){
+    for (let i = 100; i >= 3; i--) {
 
-        if(i % 7 === 0 || i % 3 === 0){
-        showResults(i);
+        if (i % 7 === 0 || i % 3 === 0) {
+            arrContagem.push(' ' + (i));
+
         }
     }
 
-} kata8();
+    showResults(nome, arrContagem)
+    return arrContagem
+
+}
+kata8();
+
+
 
 function kata9() {
-    
-    showResults('katas9');
 
-    for(let i=0; i<=100; i++){
+    let nome = 'Kata 9'
+    let arrContagem = []
 
-        if(i % 2 !== 0 && i % 5 === 0){
-            showResults(i);
+    for (let i = 0; i <= 100; i++) {
+
+        if (i % 2 !== 0 && i % 5 === 0) {
+            arrContagem.push(' ' + (i));
+
         }
     }
-    
-}kata9();
+
+    showResults(nome, arrContagem)
+    return arrContagem
+
+}
+kata9();
+
+
 
 function kata10() {
 
-    showResults('kata10');
+    let nome = 'Kata 10'
 
-    showResults(sampleArray)
+    let arrContagem = sampleArray
 
-} kata10();
+    showResults(nome, arrContagem)
+    return arrContagem
+
+}
+kata10();
+
+
 
 function kata11() {
 
-    showResults('kata11');
+    let nome = 'Kata 11'
+    let arrContagem = []
 
-    for(i = 0; i <= sampleArray.length; i++){
+    for (i = 0; i <= sampleArray.length; i++) {
 
-        if(sampleArray[i] % 2 === 0){
-            showResults(sampleArray[i]);
+        if (sampleArray[i] % 2 === 0) {
+            arrContagem.push(sampleArray[i])
         }
     }
-} kata11();
+
+    showResults(nome, arrContagem)
+    return arrContagem
+
+}
+kata11();
+
+
 
 function kata12() {
 
-    showResults('kata12');
+    let nome = 'Kata 12'
+    let arrContagem = [];
 
-    for(i = 0; i <= sampleArray.length; i++){
+    for (i = 0; i < sampleArray.length; i++) {
 
-        if(sampleArray[i] % 2 !== 0){
-            showResults(sampleArray[i]);
+        if (sampleArray[i] % 2 !== 0) {
+            arrContagem.push(' ' + (sampleArray[i]));
         }
     }
 
-} kata12();
+    showResults(nome, arrContagem)
+    return arrContagem
+
+}
+kata12();
+
+
 
 function kata13() {
 
-    showResults('kata13');
+    let nome = 'Kata 13'
+    let arrContagem = [];
 
-    for(i = 0; i <= sampleArray.length; i++){
+    for (i = 0; i <= sampleArray.length; i++) {
 
-        if(sampleArray[i] % 8 === 0){
-            showResults(sampleArray[i]);
+        if (sampleArray[i] % 8 === 0) {
+            arrContagem.push(' ' + (sampleArray[i]));
         }
     }
-    // implemente o código do kata 13 aqui
-} kata13();
+
+    showResults(nome, arrContagem)
+    return arrContagem
+}
+kata13();
+
+
 
 function kata14() {
 
-    showResults('kata14');
+    let nome = 'Kata 14'
+    let arrContagem = [];
 
-    for(i = 0; i < sampleArray.length; i++){
+    for (i = 0; i < sampleArray.length; i++) {
 
-        
-            showResults((sampleArray[i] ** 2));
-        
+
+        arrContagem.push(' ' + ((sampleArray[i] ** 2)));
+
     }
 
-} kata14();
+    showResults(nome, arrContagem)
+    return arrContagem
+
+}
+kata14();
+
+
 
 let sum = 0;
 
 function kata15() {
-    showResults('kata15');
 
-    
+    let nome = 'Kata 15'
+    let arrContagem = [];
 
-    for(i = 0; i <= 20; i++){
+    for (i = 0; i <= 20; i++) {
 
         sum += i;
-    }
-    showResults(sum);
 
-} kata15();
+    }
+    arrContagem.push(sum);
+
+    showResults(nome, arrContagem)
+    return arrContagem
+
+}
+kata15();
+
+
 
 function kata16() {
-    showResults('kata16');
 
-    for(i = 0; i < sampleArray.length; i++ ){
+    let nome = 'Kata 16'
+    let arrContagem = [];
+
+    for (i = 0; i < sampleArray.length; i++) {
 
         sum += sampleArray[i];
     }
+    arrContagem.push(sum)
 
-    showResults(sum)
+    showResults(nome, arrContagem)
+    return arrContagem
 
-} kata16();
+}
+kata16();
+
+
 
 function kata17() {
-    showResults('kata17');
+
+
+    let nome = 'Kata 17'
+    let arrContagem = [];
 
     let minor = sampleArray[0];
 
-    for(let i = 0; i <= sampleArray.length; i++){
-        if(minor > sampleArray[i]){
+    for (let i = 0; i <= sampleArray.length; i++) {
+        if (minor > sampleArray[i]) {
             minor = sampleArray[i];
         }
 
-        
+
     }
 
-    showResults(minor);
+    arrContagem.push(minor);
 
-} kata17();
+    showResults(nome, arrContagem)
+    return arrContagem
+
+}
+kata17();
+
+
 
 function kata18() {
-    showResults('kata18');
+
+    let nome = 'Kata 18'
+    let arrContagem = [];
 
     let greater = sampleArray[0];
 
-    for(let i = 0; i <= sampleArray.length; i++){
-        if(greater < sampleArray[i]){
+    for (let i = 0; i <= sampleArray.length; i++) {
+        if (greater < sampleArray[i]) {
             greater = sampleArray[i];
         }
 
-        
+
     }
 
-    showResults(greater);
+    arrContagem.push(greater);
 
-} kata18();
+    showResults(nome, arrContagem)
+    return arrContagem
 
-/**
- * Daqui em diante são os bônus, por sua conta e risco
- */
-
-function kataBonus1() {
-    // implemente o código do kata bonus 1 aqui
 }
-
-function kataBonus2() {
-    // implemente o código do kata bonus 2 aqui
-}
-
-function kataBonus3() {
-    // implemente o código do kata bonus 3 aqui
-}
-
-function kataBonus4() {
-    // implemente o código do kata bonus 4 aqui
-}
-
-function kataBonus5() {
-    // implemente o código do kata bonus 5 aqui
-}
+kata18();
